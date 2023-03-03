58a5645bc262fc05f1e7a81f8403b0eadd62c351
bepnos <langeraertmataro@gmail.com>
2023-03-03 17:44:46 +0100

    Commit 2

---
+++ Oefenzitting2/oz2/oef2.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+// Calculates factorial of a number between 1 and 10
+
+
+int main() {
+    int input;
+    printf("Give a number between 1 and 10 \n ");
+    scanf("&d", &input);
+    int result;
+    result = 1;
+    for ()
+    return 0;
+}
